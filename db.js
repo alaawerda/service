13cda858866f1a3ecd3c78b9109429e8e5b23b17
Alaa Werda <alaa.werda@medius.com>
2025-04-14 21:35:18 +0100

    add prod env db2

--- db.js
+++ db.js
@@ -1,24 +1,14 @@
 const mysql = require('mysql2/promise');
-
-/*const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'wecount',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});*/
-
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: 'mysql-34b2dc40-ala-ff3b.f.aivencloud.com',
-  user: 'avnadmin',
-  password: 'AVNS_FJeqAnK-TVYrzmalQn4',
-  database: 'wecount',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  queueLimit: parseInt(process.env.DB_QUEUE_LIMIT) || 0
 });
 
 module.exports = {
