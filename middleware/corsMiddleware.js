1e870c0d8a5a47901d84c92f8bca9ae2e4f13a85
Alaa Werda <alaa.werda@medius.com>
2025-04-24 13:50:29 +0100

    index html

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -2,10 +2,10 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: '*', // Allow all origins in development
+  origin: true, // reflect request origin for credentials
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: '*', // Allow all headers
+  // removed allowedHeaders to use dynamic reflection
   exposedHeaders: ['Set-Cookie', 'X-Json', 'Content-Type']
 };
 
@@ -14,9 +14,12 @@ const corsMiddleware = cors(corsOptions);
 
 // Secondary middleware to ensure headers are set on all responses
 const additionalCorsHeaders = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  const origin = req.headers.origin;
+  if (origin) res.header('Access-Control-Allow-Origin', origin);
+  const reqHeaders = req.headers['access-control-request-headers'];
+  if (reqHeaders) res.header('Access-Control-Allow-Headers', reqHeaders);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 };
 
